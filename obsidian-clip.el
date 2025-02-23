950718619ee0ff0b8711d6552a01f4c33a3f3bc0
ddddddeon <opfunk@gmail.com>
2025-02-23 17:05:46 -0500

    tweak comments

--- obsidian-clip.el
+++ obsidian-clip.el
@@ -13,37 +13,36 @@
 ;;; Code:
 
 (defgroup obsidian-clip nil
-  "Quickly clip code snippets to Obsidian notes."
+  "Quickly clip code snippets to Obsidian notes"
   :group 'tools
   :prefix "obsidian-clip-")
 
 (defcustom obsidian-clip-directory "~/obsidian"
-  "Directory where Obsidian vault is located."
+  "Directory where Obsidian vault is located"
   :type 'directory
   :group 'obsidian-clip)
 
 (defcustom obsidian-clip-default-file "Inbox"
-  "Default file name to use for clipping."
+  "Default file name to use for clipping"
   :type 'string
   :group 'obsidian-clip)
 
-(defvar obsidian-clip--most-recent-file obsidian-clip-default-file
-  "Most recently used file for clipping.")
+(defvar obsidian-clip-most-recent-file obsidian-clip-default-file
+  "Most recently used file for clipping")
 
 (defun obsidian-clip ()
   "Clip selected region to an Obsidian markdown file.
-If region is selected, prompts for a file name and writes the region
-as a code block to the specified markdown file in the Obsidian directory."
+Defaults first to default-file, then to most recent file to which a clip was sent." 
   (interactive)
   (if (use-region-p)
       (let* ((content (buffer-substring (region-beginning) (region-end)))
              (ext (file-name-extension (buffer-file-name)))
-             (md-file-name (read-from-minibuffer "filename: " obsidian-clip--most-recent-file))
+             (md-file-name (read-from-minibuffer "filename: " obsidian-clip-most-recent-file))
              (md-file (expand-file-name (concat md-file-name ".md") obsidian-clip-directory)))
         (with-temp-buffer
           (insert "\n```" ext "\n" content "```\n\n")
           (write-region (point-min) (point-max) md-file t))
-        (setq obsidian-clip--most-recent-file md-file-name)
+        (setq obsidian-clip-most-recent-file md-file-name)
         (find-file md-file))
     (message "No region selected")))
 
