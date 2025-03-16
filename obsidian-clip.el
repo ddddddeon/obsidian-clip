4849042c41ccf51819243ea14a86e338985d5eab
ddddddeon <opfunk@gmail.com>
2025-03-15 22:58:43 -0400

    add obsidian-clip-new-task

--- obsidian-clip.el
+++ obsidian-clip.el
@@ -1,4 +1,4 @@
-;;; obsidian-clip.el --- Clip code snippets to Obsidian notes  -*- lexical-binding: t; -*-
+;;; Obsidian-clip.el --- Clip code snippets to Obsidian notes  -*- lexical-binding: t; -*-
 
 ;; Author: Chris d'Eon 
 ;; Version: 0.1
@@ -24,18 +24,25 @@
   :type 'string
   :group 'obsidian-clip)
 
+(defcustom obsidian-clip-tasks-file "Tasks.md"
+  "Default file name to insert tasks"
+  :type 'string
+  :group 'obsidian-clip)
+
 (defvar obsidian-clip-most-recent-file obsidian-clip-default-file
   "Most recently used file for clipping")
 
 ;;;###autoload
-(defun obsidian-clip ()
+(defun obsidian-clip (md-file-name)
   "Clip selected region to an Obsidian markdown file.
 Defaults first to default-file, then to most recent file to which a clip was sent." 
-  (interactive)
+  (interactive "s> ")
   (if (use-region-p)
       (let* ((content (buffer-substring (region-beginning) (region-end)))
              (ext (file-name-extension (buffer-file-name)))
-             (md-file-name (read-from-minibuffer "filename: " obsidian-clip-most-recent-file))
+             (md-file-name (if (string= md-file-name "")
+                              obsidian-clip-most-recent-file
+                            md-file-name))
              (md-file (expand-file-name (concat md-file-name ".md") obsidian-clip-directory)))
         (with-temp-buffer
           (insert "\n```" ext "\n" content "```\n\n")
@@ -43,6 +50,19 @@ Defaults first to default-file, then to most recent file to which a clip was sen
         (setq obsidian-clip-most-recent-file md-file-name)
         (find-file md-file))
     (message "No region selected")))
+
+;;;###autoload
+(defun obsidian-clip-new-task (task)
+  (interactive "s> ")
+  (let* ((tasks-file (concat obsidian-clip-directory "/" obsidian-clip-tasks-file)))
+    (with-temp-buffer
+      (when (file-exists-p tasks-file)
+        (insert-file-contents tasks-file))
+      (goto-char (point-max))
+      (insert "\n- [ ] " task)
+      (write-region (point-min) (point-max) tasks-file))
+    (find-file-other-window tasks-file)
+    (goto-char (point-max))))
 
 (provide 'obsidian-clip)
 
